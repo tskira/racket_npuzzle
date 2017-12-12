ead8c9b317fbae4ca917344ed5f1798fa0cf4f14
tskira <tskira@hotmail.com>
2017-12-11 22:49:19 -0200

    fazer menu

--- board.rkt
+++ board.rkt
@@ -1,9 +1,12 @@
 #lang racket
 
-(require 2htdp/image 2htdp/universe)
+(require 2htdp/universe)
+;;; (require 2htdp/image)
+(require (prefix-in htdp: 2htdp/image))
+(require racket/gui)
 (require lang/posn)
 
-(define n 2)
+(define n 3)
 (define size-cell 100)
 (define text-size (* size-cell 0.5))
 
@@ -16,27 +19,27 @@
 (vector-set*! final-config (- (* n n) 1) 0)
 
 (define end-game-message 
-    (text "GG WP" text-size "red"))
+    (htdp:text "GG WP" text-size "red"))
 
 (define start-menu
-    (text "Entre com o tamanho do jogo: (ex 4)" (* text-size 0.4)"red"))
+    (htdp:text "Entre com o tamanho do jogo: (ex 4)" (* text-size 0.4)"red"))
 
 (define start-layout
-    (empty-scene 400 500))
+    (htdp:empty-scene 400 500))
 
 (define cell
-    (square size-cell "outline" "black"))
+    (htdp:square size-cell "outline" "black"))
 
 (define (number-text an-number)
     (cond
-        [(= an-number 0) (text " " text-size "blue")]
-        [else (text (number->string an-number) text-size "blue")]))
+        [(= an-number 0) (htdp:text " " text-size "blue")]
+        [else (htdp:text (number->string an-number) text-size "blue")]))
 
 (define (size-of-game)
     (* (- n 1) (- n 1)))
 
 (define layout
-    (empty-scene (* n size-cell) (* n size-cell) ))
+    (htdp:empty-scene (* n size-cell) (* n size-cell) ))
 
 (define (check-move current-position movement config)
     (and (< (+ current-position movement) (vector-length config))
@@ -76,8 +79,8 @@
     [current-config-text (build-list (* n n) (lambda (x) (number-text (vector-ref current-config x))))]
     [cells-position (build-list  (* n n) (lambda (x) (make-posn (+ (* (remainder x n) size-cell) (/ size-cell 2)) (+ (* (quotient x n) size-cell) (/ size-cell 2)))))])
   (cond
-  [(game-over w) (place-image end-game-message (/ (* n size-cell) 2) (/ (* n size-cell) 2) layout)]
-  [else (place-images
+  [(game-over w) (htdp:place-image end-game-message (/ (* n size-cell) 2) (/ (* n size-cell) 2) layout)]
+  [else (htdp:place-images
     (append cells-number current-config-text)
     (append cells-position cells-position)
     layout
@@ -86,7 +89,7 @@
 )
 
 (define (draw-menu-scene w)
-    (place-image/align start-menu 200 50 "center" "center" start-layout)
+    (htdp:place-image/align start-menu 200 50 "center" "center" start-layout)
 )
 
 (define (generate-start-config current-config)
