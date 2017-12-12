8431d466370e84c813514693dadc34eaf0b411be
tskira <tskira@hotmail.com>
2017-12-12 13:14:25 -0200

    Menu ok

--- board.rkt
+++ board.rkt
@@ -1,132 +1,149 @@
 #lang racket
 
 (require 2htdp/universe)
-;;; (require 2htdp/image)
 (require (prefix-in htdp: 2htdp/image))
 (require racket/gui)
 (require lang/posn)
 
-(define n 3)
-(define size-cell 100)
-(define text-size (* size-cell 0.5))
+(define (play n player)
 
-(define current-config (build-vector (* n n) add1))
+    (define n-moves 0)
 
-(define swap-config (build-vector (* n n) add1))
-(vector-set*! swap-config (- (* n n) 1) 0)
+    (define size-cell 50)
 
-(define final-config (build-vector (* n n) add1))
-(vector-set*! final-config (- (* n n) 1) 0)
+    (define text-size (* size-cell 0.5))
 
-(define end-game-message 
-    (htdp:text "GG WP" text-size "red"))
+    (define current-config (build-vector (* n n) add1))
 
-(define start-menu
-    (htdp:text "Entre com o tamanho do jogo: (ex 4)" (* text-size 0.4)"red"))
+    (define swap-config (build-vector (* n n) add1))
+    (vector-set*! swap-config (- (* n n) 1) 0)
 
-(define start-layout
-    (htdp:empty-scene 400 500))
+    (define final-config (build-vector (* n n) add1))
+    (vector-set*! final-config (- (* n n) 1) 0)
 
-(define cell
-    (htdp:square size-cell "outline" "black"))
+    (define end-game-message 
+        (htdp:text "GG WP" text-size "red"))
 
-(define (number-text an-number)
-    (cond
-        [(= an-number 0) (htdp:text " " text-size "blue")]
-        [else (htdp:text (number->string an-number) text-size "blue")]))
+    (define start-menu
+        (htdp:text "Entre com o tamanho do jogo: (ex 4)" (* text-size 0.4)"red"))
 
-(define (size-of-game)
-    (* (- n 1) (- n 1)))
+    (define start-layout
+        (htdp:empty-scene 400 500))
 
-(define layout
-    (htdp:empty-scene (* n size-cell) (* n size-cell) ))
+    (define cell
+        (htdp:square size-cell "outline" "black"))
 
-(define (check-move current-position movement config)
-    (and (< (+ current-position movement) (vector-length config))
-         (>= (+ current-position movement) 0) (or (= (quotient current-position n) (quotient (+ current-position movement) n)) (= (remainder current-position n) (remainder (+ current-position movement) n))))
+    (define (number-text an-number)
+        (cond
+            [(= an-number 0) (htdp:text " " text-size "blue")]
+            [else (htdp:text (number->string an-number) text-size "blue")]))
+
+    (define (size-of-game)
+        (* (- n 1) (- n 1)))
+
+    (define layout
+        (htdp:empty-scene (* n size-cell) (* n size-cell) ))
+
+    (define (check-move current-position movement config)
+        (and (< (+ current-position movement) (vector-length config))
+            (>= (+ current-position movement) 0) (or (= (quotient current-position n) (quotient (+ current-position movement) n)) (= (remainder current-position n) (remainder (+ current-position movement) n))))
+        )
+    (define (increment!)
+        (set! n-moves (add1 n-moves ) )
     )
-    
-(define (move size)
-    (lambda (w current-config)
-        (define zero-position (vector-member 0 current-config))
-        (cond 
-        [(check-move zero-position size current-config)
-            (let ([swap-value (vector-ref current-config (+ zero-position size))])
-                (vector-set*! current-config zero-position swap-value)
-                (vector-set*! current-config (+ size zero-position) 0)) 
-        ]
-        )))
 
-(define move-right (move -1))
-(define move-left (move 1))
-(define move-up (move n))
-(define move-down (move (- n)))
+    (define (move size)
+        (lambda (w current-config)
+            (define zero-position (vector-member 0 current-config))
+            (cond 
+            [(check-move zero-position size current-config)
+                (let ([swap-value (vector-ref current-config (+ zero-position size))])
+                    (vector-set*! current-config zero-position swap-value)
+                    (vector-set*! current-config (+ size zero-position) 0)
+                    (increment!)
+                 ) 
+            ]
+            )))
 
-(define (key-function w key)
+    (define move-right (move -1))
+    (define move-left (move 1))
+    (define move-up (move n))
+    (define move-down (move (- n)))
+
+    (define (key-function w key)
+        (cond
+            [(key=? key "right") (move-right w current-config)]
+            [(key=? key "left") (move-left w current-config)]
+            [(key=? key "up") (move-up w current-config)]
+            [(key=? key "down") (move-down w current-config)]))
+
+    (define (game-over w)
+        (equal? current-config final-config)
+    )
+
+    (define (draw-scene w)
+    (let
+        ([cells-number (build-list  (* n n) (lambda (x) cell))]
+        [current-config-text (build-list (* n n) (lambda (x) (number-text (vector-ref current-config x))))]
+        [cells-position (build-list  (* n n) (lambda (x) (make-posn (+ (* (remainder x n) size-cell) (/ size-cell 2)) (+ (* (quotient x n) size-cell) (/ size-cell 2)))))])
     (cond
-        [(key=? key "right") (move-right w current-config)]
-        [(key=? key "left") (move-left w current-config)]
-        [(key=? key "up") (move-up w current-config)]
-        [(key=? key "down") (move-down w current-config)]))
+    [(game-over w) (htdp:place-image end-game-message (/ (* n size-cell) 2) (/ (* n size-cell) 2) layout)]
+    [else (htdp:place-images
+        (append cells-number current-config-text)
+        (append cells-position cells-position)
+        layout
+        )]
+        ))
+    )
 
-(define (game-over w)
-    (equal? current-config final-config)
-)
+    (define (draw-menu-scene w)
+        (htdp:place-image/align start-menu 200 50 "center" "center" start-layout)
+        (cond
+            [(game-over w) (sleep 3)]
+        )
+    )
 
-(define (draw-scene w)
-(let
-    ([cells-number (build-list  (* n n) (lambda (x) cell))]
-    [current-config-text (build-list (* n n) (lambda (x) (number-text (vector-ref current-config x))))]
-    [cells-position (build-list  (* n n) (lambda (x) (make-posn (+ (* (remainder x n) size-cell) (/ size-cell 2)) (+ (* (quotient x n) size-cell) (/ size-cell 2)))))])
-  (cond
-  [(game-over w) (htdp:place-image end-game-message (/ (* n size-cell) 2) (/ (* n size-cell) 2) layout)]
-  [else (htdp:place-images
-    (append cells-number current-config-text)
-    (append cells-position cells-position)
-    layout
-    )]
-    ))
-)
-
-(define (draw-menu-scene w)
-    (htdp:place-image/align start-menu 200 50 "center" "center" start-layout)
-)
-
-(define (generate-start-config current-config)
-    (for ([i (in-range 1000)])
-            (let 
-                ([x (random 4)])
-                (cond 
-                    [(= x 0) (move-right 0 current-config)]
-                    [(= x 1) (move-left 0 current-config)]
-                    [(= x 2) (move-up 0 current-config)]
-                    [(= x 3) (move-down 0 current-config)]
+    (define (generate-start-config current-config)
+        (for ([i (in-range (* n 300))])
+                (let 
+                    ([x (random 4)])
+                    (cond 
+                        [(= x 0) (move-right 0 current-config)]
+                        [(= x 1) (move-left 0 current-config)]
+                        [(= x 2) (move-up 0 current-config)]
+                        [(= x 3) (move-down 0 current-config)]
+                    )
                 )
-            )
+        )
+        current-config
     )
-    current-config
+
+    (vector-copy! current-config 0 (generate-start-config swap-config))
+
+    
+    (define (start-game)
+        (set! n-moves 0)
+        (big-bang 0
+            (name "N-PUZZLE")
+            (on-key key-function)
+            (to-draw draw-scene)
+            (stop-when game-over draw-scene)
+        )
+        (display n-moves)
+    )
+    (print player)
+    (start-game)
 )
 
-;;; (define (get-entry w key)
-;;;     (cond
-;;;         [(key=? key "right") (move-right w current-config)]
-;;;         [(key=? key "left") (move-left w current-config)]
-;;;         [(key=? key "up") (move-up w current-config)]
-;;;         [(key=? key "down") (move-down w current-config)])
-;;;         [(number? key) ()]        
-;;; )
+(define entry-menu (instantiate dialog% ("N-PUZZLE")))
+ 
+(define n-size (new text-field% [parent entry-menu] [label "Tamanho"]))
+(define player-name (new text-field% [parent entry-menu] [label "Jogador"]))
 
-(vector-copy! current-config 0 (generate-start-config swap-config))
-current-config
+(define panel (new horizontal-panel% [parent entry-menu]
+                                     [alignment '(center center)]))
+ 
+(new button% [parent panel] [label "Proximo"]
+    [callback (lambda (button event)  (send entry-menu show #f) (play (string->number (send n-size get-value)) (send player-name get-value)))])
 
-;;; (big-bang 0
-;;;     (to-draw draw-menu-scene)
-;;;     (on-key get-entry)
-;;; )
-
-(big-bang 0 
-    (name "N-PUZZLE")
-    (on-key key-function)
-    (to-draw draw-scene)
-    (stop-when game-over draw-scene)
-)
+(send entry-menu show #t)
